66f790ee18cc4d4d0120beb8b882e90ee1632268
Shuvajyoti99 <shuvajyoti99@gmail.com>
2020-04-19 12:00:03 +0530

    Updates

---
+++ precache-manifest.1e7228a6010a97e464401420c4ec13cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09c9bf4cfd7c1334f21c8c0aba25d40d",
+    "url": "/react-deploy2/index.html"
+  },
+  {
+    "revision": "0800a8f736d46cf17e73",
+    "url": "/react-deploy2/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "b0fc344d8f52820a6329",
+    "url": "/react-deploy2/static/css/main.0d16e165.chunk.css"
+  },
+  {
+    "revision": "0800a8f736d46cf17e73",
+    "url": "/react-deploy2/static/js/2.df3de0a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy2/static/js/2.df3de0a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0fc344d8f52820a6329",
+    "url": "/react-deploy2/static/js/main.62fe0d38.chunk.js"
+  },
+  {
+    "revision": "0c593067f2b3ff45433a",
+    "url": "/react-deploy2/static/js/runtime-main.e38a9ba3.js"
+  }
+]);
